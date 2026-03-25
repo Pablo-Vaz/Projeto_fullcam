91e4194392660d7d71fc708f87c9aa5625e5409c
FullCam <pablovaz37@gmail.com>
2026-03-25 11:42:40 -0300

    update consumer.py

--- app/services/consumer.py
+++ app/services/consumer.py
@@ -13,33 +13,42 @@ load_dotenv()
 RABBITMQ_URL = os.getenv("RABBITMQ_URL")
 
 
-def consumir():
-    while True:
-        try:
-            connection_parameters = pika.URLParameters(RABBITMQ_URL)
-            
-            channel = pika.BlockingConnection(connection_parameters).channel()
+class RabbitMqConsumer:
+    def __init__(self, filas):
+        self.filas = filas
+        self.connection = None
+        self.channel = None
 
-            channel.queue_declare(queue='eventos_queue', durable=True)
-            channel.queue_declare(queue='queue_crud', durable=True)
+    def conection(self):
+        connection_parameters = pika.URLParameters(RABBITMQ_URL)
+        self.connection = pika.BlockingConnection(connection_parameters)
+        self.channel = self.connection.channel()
 
-            channel.basic_qos(prefetch_count=1)
+        for fila in self.filas:
+            self.channel.queue_declare(queue=fila, durable=True)
 
-            channel.basic_consume(queue='eventos_queue', on_message_callback=callback, auto_ack=False)
-            channel.basic_consume(queue='queue_crud', on_message_callback=callback, auto_ack=False)
+        self.channel.basic_qos(prefetch_count=1)
 
-            channel.start_consuming()
-        except:
-            time.sleep(5)
-            continue
+    def callback(self, ch, method, properties, body):
+        evento = json.loads(body)
+        salvar(evento)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
 
-def callback(ch, method, properties, body):
-    
-    evento = json.loads(body)
-    salvar(evento)
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-       
+    def consumir(self):
+        while True:
+            try:
+                self.conection()
 
+                for fila in self.filas:
+                    self.channel.basic_consume(queue=fila, on_message_callback=self.callback, auto_ack=False)
+
+                self.channel.start_consuming()
+            except:
+                time.sleep(5)
+            finally:
+                if self.channel and self.channel.is_open:
+                    self.channel.stop_consuming()
 
 if __name__ == "__main__":
-    consumir()
+    consumer = RabbitMqConsumer(['eventos_queue', 'queue_crud'])
+    consumer.consumir()
